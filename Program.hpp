7b79c258b3467ebe65ba399b1bb3238615e4feb1
Oliver Tongrak <74416671+tongrak@users.noreply.github.com>
2021-03-04 23:15:14 +0700

    adding event loading, checking and display
    still under construction, tho.

--- Program.hpp
+++ Program.hpp
@@ -1,12 +1,11 @@
 #ifndef PROGRAM_HPP
 #define PROGRAM_HPP
 
-#include<iostream>
-#include<string>
 #include<cctype>
 using namespace std;
 
 #include "Room.hpp"
+#include "Utilities.hpp"
 
 class Program
 {
@@ -20,27 +19,35 @@ class Program
 
     void Action_checking();
     bool CheckingInputDir();
+    void Event_checking();
+
     void SplitKeyWord(string ,string &, string &);
     string Tolower(string);
 
 };
 
 //constructor
+Room Room_calling;
+
 Program::Program(){
     holder="",in_key="",in_wanted="";
     is_running=true;
+    Room_calling.LoadMaps();
+    Room_calling.LoadEvent();
+    Room_calling.ChangeCurr_id(1);
+    //Room_calling.ShowingRoomInfo();
 }
 
 //Checking for userinput, if key word is pasent, do a "act".
 void Program::Action_checking(){
-    Room room;
+    Menu display;
     cout<<"> ";
     getline(cin, holder);
     SplitKeyWord(holder, in_key, in_wanted);
     if(Tolower(in_key)=="go")
     {
-        if(in_wanted=="")cout<<"go where?"<<endl;
-        else if(CheckingInputDir())room.ShowingRoomInfo();
+        if(in_wanted=="")cout<<"where?"<<endl;
+        else if(CheckingInputDir())Room_calling.ShowingRoomInfo();
         else{cout<<"nothing there"<<endl;}
     }
     else if(Tolower(in_key)=="exit"){
@@ -49,7 +56,8 @@ void Program::Action_checking(){
         }
     else if(Tolower(in_key)=="look")
     {
-        room.ShowingRoomInfo();
+        display.ClearScreen();
+        Room_calling.ShowingRoomInfo();
     }
     else{cout<<"Sorry not quite get that."<<endl;}
         
@@ -75,39 +83,39 @@ string Program::Tolower(string text){
 
 //checking for valid direction and change curr_id;
 bool Program::CheckingInputDir(){
-    Room R;
     string holder;
-    bool check=false;
+    bool check=true;
     in_wanted=Tolower(in_wanted);
-    if(in_wanted=="north"&&R.RuturnNeighbor(0, holder)!="NULL")
+    if(in_wanted=="north"&&Room_calling.ReturnNeighbor(0, holder)!="NULL")
     {
-        R.ChangeCurr_id(stoi(holder));
-        check=true;
+        Room_calling.ChangeCurr_id(stoi(holder));
     }
-    if(in_wanted=="east"&&R.RuturnNeighbor(1, holder)!="NULL")
+    else if(in_wanted=="east"&&Room_calling.ReturnNeighbor(1, holder)!="NULL")
     {
-        R.ChangeCurr_id(stoi(holder));
-        check=true;
+        Room_calling.ChangeCurr_id(stoi(holder));
     }
-    if(in_wanted=="west"&&R.RuturnNeighbor(2, holder)!="NULL")
+    else if(in_wanted=="west"&&Room_calling.ReturnNeighbor(2, holder)!="NULL")
     {
-        R.ChangeCurr_id(stoi(holder));
-        check=true;
+        Room_calling.ChangeCurr_id(stoi(holder));
     }
-    if(in_wanted=="south"&&R.RuturnNeighbor(3, holder)!="NULL")
+    else if(in_wanted=="south"&&Room_calling.ReturnNeighbor(3, holder)!="NULL")
     {
-        R.ChangeCurr_id(stoi(holder));
-        check=true;
+        Room_calling.ChangeCurr_id(stoi(holder));
     }
+    else{check=false;}
     return check;
 } 
 
+void Program::Event_checking()
+{
+    if(Room_calling.ReturnEventId() != "NULL")
+    {
+        
+    }
+}
+
 //for runing the 'game'
 void Program::Run(){
-    Room room_calling;
-    room_calling.LoadMaps();
-    room_calling.ChangeCurr_id(1);
-    room_calling.ShowingRoomInfo();
     do{
     Action_checking();
     }while(is_running);
--- Room.hpp
+++ Room.hpp
@@ -7,6 +7,16 @@
 #include<map>
 using namespace std;
 
+/*
+    Room.hpp duties are to:
+    -   load Maps and store into map
+    -   Load Event and store into map
+    -   Get name and description, from given id, and return them
+    -   Get Neighbor id, from given id, and return them
+    -   Change Current position ID;
+
+*/
+
 //damn include guard;
 //#include "Program.hpp"
 
@@ -17,29 +27,59 @@ struct Room{
     string name; 
     string description;
     string Neighbor[4];
+    string eventID;
 
+    void InitVar();
     void LoadMaps();
+    void LoadEvent();
     void ShowingRoomInfo();
     void SettingUpRoom(int id, string &s1, string &s2);
-    string RuturnNeighbor(int dir, string &keeper);
+    string ReturnNeighbor(int dir, string &keeper);
+    string ReturnEventId();
     void ChangeCurr_id(int id);
 
-}; 
+    //constructor and distructor
+    Room();
+    ~Room();
+
+};
+
+struct Event{
+    string event_name;
+    string event_descrip;
+    char event_type[2];
+    string event_option;
+};
 
 //declare some variable
-map<int,Room> Storage;
+map<int,Room> roomStorage;
+map<int,Event> eventStorage;
 int *Curr_id = new int;
 string *NAME = new string;
 string *DESCRIP = new string;
 
-//constructor, disable for now.
-/* Room::Room()
+void Room::InitVar()
 {
-    //LoadMaps();
     *Curr_id=0;
-} */
+    *NAME="";
+    *DESCRIP="";
+}
 
-//loading maps from Map.txt and store it in Storage map.
+//constructor, starting with Initialize some dynamics;
+Room::Room()
+{
+    this->InitVar();
+} 
+
+//distructor, delete all dynamic
+Room::~Room()
+{
+    delete Curr_id;
+    delete NAME;
+    delete DESCRIP;
+}
+
+//loading maps from Map.txt and store it in roomStorage map.
 void Room::LoadMaps()
 {
     string holder_str;
@@ -58,21 +98,52 @@ void Room::LoadMaps()
             {
                 getline(file, holder_str);
                 int temp=holder_str.find_first_of(" ");
-                string str_temp=holder_str.substr(temp+1);
-                R.Neighbor[i]=str_temp;
+                R.Neighbor[i]=holder_str.substr(temp+1);
             }
-            Storage.insert(pair<int,Room>(holder_int,R));
+            getline(cin, holder_str);
+            int temp=holder_str.find_first_of(" ");
+            R.eventID=holder_str.substr(temp+1);
+            roomStorage.insert(pair<int,Room>(holder_int,R));
+        }
+    }
+    file.close();
+}
+
+void Room::LoadEvent()
+{
+    string holder_str;
+    int holder_int;
+    ifstream file("Events.txt");
+    while(getline(file, holder_str))
+    {
+        if(holder_str=="EVENT_BEGIN")
+        {
+            Event eve;
+            getline(file, holder_str);
+            holder_int=stoi(holder_str);
+            getline(file, eve.event_name);
+            getline(file, eve.event_descrip);
+            string temp;
+            getline(file,temp);
+            strcpy(eve.event_type, temp.c_str());
+            getline(file, eve.event_option);
+            eventStorage.insert(pair<int,Event>(holder_int,eve));   
         }
     }
     file.close();
 }
 
 //Ruturing a Neighbor id.
-string Room::RuturnNeighbor(int dir, string &keeper){
-    keeper = Storage[*Curr_id].Neighbor[dir];
-    return Storage[*Curr_id].Neighbor[dir];
+string Room::ReturnNeighbor(int dir, string &keeper){
+    keeper = roomStorage[*Curr_id].Neighbor[dir];
+    return roomStorage[*Curr_id].Neighbor[dir];
 
 }
+
+string Room::ReturnEventId(){
+    return roomStorage[*Curr_id].eventID;
+}
+
 //as the name suggest, it change Curr_id
 void Room::ChangeCurr_id(int id){
     *Curr_id=id;
@@ -80,8 +151,8 @@ void Room::ChangeCurr_id(int id){
 
 //Setting up string to be print.
 void Room::SettingUpRoom(int ID, string &inni_name, string &inni_des){
-    inni_name=Storage[ID].name;
-    inni_des=Storage[ID].description;
+    inni_name=roomStorage[ID].name;
+    inni_des=roomStorage[ID].description;
 }
 
 
