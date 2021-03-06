ceccba66faf497a9bbfb330b3972285c2d05d1f6
Oliver Tongrak <74416671+tongrak@users.noreply.github.com>
2021-03-06 15:45:09 +0700

    Adding old code to new format

--- Text_Sekai/Program.h
+++ Text_Sekai/Program.h
@@ -1,2 +1,168 @@
 #pragma once
-//it not empty;
+#ifndef PROGRAM_HPP
+#define PROGRAM_HPP
+
+#include<cctype>
+using namespace std;
+
+#include "Room.h"
+//#include "Utilities.hpp"
+
+/*
+    Program.h, is almost like our main game processor, it duties are:
+    -     Get current room detail and pass it on to GUI
+    -     Taking User command, process, and return a order;
+*/
+
+/*
+    String Processing  Function
+*/
+
+//from lab17, thank prof.
+void SplitKeyWord(string in_text, string& key, string& wanted) {
+    int holder = in_text.find_first_of(" ");
+    key = in_text.substr(0, holder);
+    wanted = in_text.substr(holder + 1);
+
+}
+
+//Tolower is a cousin of ToUpperString
+string Tolower(string text) {
+    int i = text.size();
+    string out = "";
+    for (int j = 0; j < i; j++)
+    {
+        out += tolower(text[j]);
+    }
+    return out;
+}
+
+/*
+    Creating Room object
+*/
+    Room Room_calling;
+
+/*
+    Class and constructor
+*/
+
+class Program
+{
+public:
+    Program();
+    //void Run();
+
+private:
+    string holder, in_key, in_wanted;
+    bool is_running;
+
+    void Action_checking();
+    bool CheckingInputDir();
+    void Event_checking();
+
+
+};
+
+//constructor, load maps and event, and set Current ID to start
+Program::Program() {
+    holder = "", in_key = "", in_wanted = "";
+    is_running = true;
+    Room_calling.LoadMaps();
+    Room_calling.LoadEvent();
+    Room_calling.ChangeCurr_id(1);
+    //Room_calling.ShowingRoomInfo();
+}
+
+/*
+    Decision Making Function
+*/
+
+//Checking for userinput, if key word is pasent, do a "act".
+void Program::Action_checking() {
+    cout << "> ";
+    getline(cin, holder);
+    SplitKeyWord(holder, in_key, in_wanted);
+    if (Tolower(in_key) == "go")
+    {
+        if (in_wanted == "")cout << "where?" << endl;
+        else if (CheckingInputDir())/*Display detail of current room*/;
+        else { cout << "nothing there" << endl; }
+    }
+    else if (Tolower(in_key) == "exit") {
+        cout << "hope you enjoy your stay";
+        is_running = false;
+    }
+    else if (Tolower(in_key) == "look")
+    {
+        /*Display detail of current room*/;
+    }
+    else { cout << "Sorry not quite get that." << endl; }
+
+}
+
+/*
+    Checking Function
+*/
+
+//checking for valid direction and change curr_id;
+bool Program::CheckingInputDir() {
+    string holder;
+    bool check = true;
+    in_wanted = Tolower(in_wanted);
+    if (in_wanted == "north" && Room_calling.GetNeighbors(0, holder) != "NULL")
+    {
+        Room_calling.ChangeCurr_id(stoi(holder));
+    }
+    else if (in_wanted == "east" && Room_calling.GetNeighbors(1, holder) != "NULL")
+    {
+        Room_calling.ChangeCurr_id(stoi(holder));
+    }
+    else if (in_wanted == "west" && Room_calling.GetNeighbors(2, holder) != "NULL")
+    {
+        Room_calling.ChangeCurr_id(stoi(holder));
+    }
+    else if (in_wanted == "south" && Room_calling.GetNeighbors(3, holder) != "NULL")
+    {
+        Room_calling.ChangeCurr_id(stoi(holder));
+    }
+    else { check = false; }
+    return check;
+}
+
+void Program::Event_checking()
+{
+    string holder;
+    if (Room_calling.GetEventId(holder) != "NULL")
+    {
+        switch (Room_calling.GetEventType(stoi(holder)))
+        {
+        case'C':
+            /*Enter Combat*/
+            break;
+        case'B':
+            /*Take Curr event description, display, and wait for answer*/
+            break;
+        case'S':
+            /*Take Curr event description and display*/
+            break;
+        default:
+            break;
+        }
+    }
+}
+
+
+
+/*
+//for runing the 'game'
+void Program::Run() {
+    do {
+        Action_checking();
+    } while (is_running);
+
+}
+*/
+
+
+
+#endif
--- Text_Sekai/Room.h
+++ Text_Sekai/Room.h
@@ -1,2 +1,207 @@
 #pragma once
-//Not empty
+#ifndef ROOM_HPP
+#define ROOM_HPP
+
+#include<iostream>
+#include<fstream> 
+#include<string>
+#include<map>
+using namespace std;
+
+/*
+    Room.h duties are to:
+    -   load Maps detail and store into Map type arrays
+    -   Load Event detail and store into Map type arrays
+    -   Returning Name and Description, for a  given room id, in string type
+    -   Returning Neightbor IDs in string type
+    -   Taking ID and change Current ID
+
+*/
+
+/*
+    Structure of Room and Event
+*/
+
+struct Room {
+    string name;
+    string description;
+    string Neighbor[4];
+    string eventID;
+
+    void InitVar();
+    void LoadMaps();
+    void LoadEvent();
+    //void ShowingRoomInfo();
+    //void GetRoomStr(int id, string& s1, string& s2);
+    string GetCurrName();
+    string GetCurrDes();
+    char GetEventType(int );
+    string GetNeighbors(int dir, string& keeper);
+    string GetEventId(string&);
+    void ChangeCurr_id(int id);
+
+    //constructor and distructor
+    Room();
+    ~Room();
+
+};
+
+struct Event {
+    string event_name;
+    string event_descrip;
+    char event_type;
+    string event_option;
+};
+
+//declare some variable
+map<int, Room> roomStorage;
+map<int, Event> eventStorage;
+int* Curr_id = new int;
+string* NAME = new string;
+string* DESCRIP = new string;
+
+
+/*
+    Constructor and Distructor of structure
+*/
+
+//Initialize variables in needs.
+void Room::InitVar()
+{
+    *Curr_id = 0;
+    *NAME = "";
+    *DESCRIP = "";
+}
+
+//constructor, starting with Initialize some dynamics;
+Room::Room()
+{
+    this->InitVar();
+}
+
+//distructor, delete all dynamic
+Room::~Room()
+{
+    delete Curr_id;
+    delete NAME;
+    delete DESCRIP;
+}
+
+/*
+    Loading zone
+*/
+
+//loading maps from Map.txt and store it in roomStorage map.
+void Room::LoadMaps()
+{
+    string holder_str;
+    int holder_int;
+    ifstream file("Maps.txt");
+    while (getline(file, holder_str))
+    {
+        if (holder_str == "ROOM_BEGIN")
+        {
+            Room R;
+            getline(file, holder_str);
+            holder_int = stoi(holder_str);
+            getline(file, R.name);
+            getline(file, R.description);
+            for (int i = 0; i < 4; i++)
+            {
+                getline(file, holder_str);
+                int temp = holder_str.find_first_of(" ");
+                R.Neighbor[i] = holder_str.substr(temp + 1);
+            }
+            getline(cin, holder_str);
+            int temp = holder_str.find_first_of(" ");
+            R.eventID = holder_str.substr(temp + 1);
+            roomStorage.insert(pair<int, Room>(holder_int, R));
+        }
+    }
+    file.close();
+}
+
+//Loading event detail from Event.txt and store it in eventStorage Map.
+void Room::LoadEvent()
+{
+    string holder_str;
+    int holder_int;
+    ifstream file("Events.txt");
+    while (getline(file, holder_str))
+    {
+        if (holder_str == "EVENT_BEGIN")
+        {
+            Event eve;
+            getline(file, holder_str);
+            holder_int = stoi(holder_str);
+            getline(file, eve.event_name);
+            getline(file, eve.event_descrip);
+            string temp;
+            getline(file, temp);
+            eve.event_type = temp[0];
+            getline(file, eve.event_option);
+            eventStorage.insert(pair<int, Event>(holder_int, eve));
+        }
+    }
+    file.close();
+}
+
+/*
+    Accesor and Changing on command function
+*/
+
+//Ruturing a Neighbor id.
+string Room::GetNeighbors(int dir, string& keeper) {
+    keeper = roomStorage[*Curr_id].Neighbor[dir];
+    return roomStorage[*Curr_id].Neighbor[dir];
+
+}
+
+//Returning a current event id.
+string Room::GetEventId(string &keeper) {
+    keeper = roomStorage[*Curr_id].eventID;
+    return roomStorage[*Curr_id].eventID;
+}
+
+//Taken ID and change Current ID
+void Room::ChangeCurr_id(int id) {
+    *Curr_id = id;
+}
+
+/*
+//Setting up string to be print.
+void Room::GetRoomStr(int ID, string& inni_name, string& inni_des) {
+    inni_name = roomStorage[ID].name;
+    inni_des = roomStorage[ID].description;
+}
+*/
+
+//returning Current room name, in string
+string Room::GetCurrName()
+{
+    return roomStorage[*Curr_id].name;
+}
+
+
+//returning Current room description, in string
+string Room::GetCurrDes()
+{
+    return roomStorage[*Curr_id].description;
+}
+
+char Room::GetEventType(int key)
+{
+    return eventStorage[key].event_type;
+}
+
+
+/*
+//for printing string on console(for now);
+void Room::ShowingRoomInfo()
+{
+    SettingUpRoom(*Curr_id, *NAME, *DESCRIP);
+    cout << "\t" << *NAME << endl << *DESCRIP << endl;
+}
+*/
+
+#endif
